d5fccd93ced8db339a7597d1e78b877b2bb8d695
Kevin Baugh <loraxx753@gmail.com>
2012-10-19 00:16:27 -0400

    added fix for chat scroll

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -31,7 +31,7 @@ function heartbeat() {
 		if(data.chat)
 		{
 			$('#chat').html(data.chat);
-			$("#chat").animate({ scrollTop: $("#chat").height() });
+			$("#chat").animate({ scrollTop: $("#chat")[0].scrollHeight });
 		}
 
 	}, 'json');
@@ -147,7 +147,7 @@ $(document).ready(function() {
 			$.post('/game/chat/'+hash, {'text' : text}, function(data) {
 				$this.val('');
 				$('#chat').html(data);
-				$("#chat").animate({ scrollTop: $("#chat").height() });
+				$("#chat").animate({ scrollTop: $("#chat")[0].scrollHeight });
 			});
 		}
 	});
